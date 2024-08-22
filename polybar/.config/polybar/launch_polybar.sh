a79a5c805922ec863cd20ee95ed1be9323857381
Uurtsaikh Nyambat <vvrtsaix.n@gmail.com>
2024-08-22 23:45:03 +0800

    fixed i3 config

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+# Bar is the name set in the polybar config, so if you change it, you have to change it here too.
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar --reload toph &
@@ -5,3 +15,5 @@ if type "xrandr"; then
 else
     polybar --reload toph &
 fi
+
+echo "Bars launched..."
